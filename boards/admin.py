4a1ea7ea73ac4bac04662c89abf0c7ad1c3e8149
Dernov Ilya <dernov@vitaexpress.ru>
2019-03-28 14:41:10 +0400

    admin fixes

--- boards/admin.py
+++ boards/admin.py
@@ -2,9 +2,21 @@ from django.contrib import admin
 from .models import Board, Thread
 
 class BoardAdmin(admin.ModelAdmin):
-    list_display = ('id', 'boardName', 'isActive')
-    list_filter = ['isActive']
+    list_display = ('id', 'boardName', 'activity')
+
+    def activity(self, obj):
+        return 'YES' if obj.isActive > 0 else 'NO'
+
+
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ('id', 'threadTitle', 'getboard', 'activity')
+
+    def activity(self, obj):
+        return 'YES' if obj.isActive > 0 else 'NO'
+    def getboard(self, obj):
+        return obj.boardFK.boardName
+
 
 # Register your models here.
 admin.site.register(Board, BoardAdmin)
-admin.site.register(Thread)
+admin.site.register(Thread, ThreadAdmin)
